2b84c8ba6e8bc50b6bde74c9cc07e6a02b471817
NetAcad DEVASC <changeme@example.com>
2026-05-27 19:27:12 +0000

    Corregir creacion de directorios en el script

--- sample-app.sh
+++ sample-app.sh
@@ -1,22 +1,20 @@
 rm -rf tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
+mkdir -p tempdir/templates
+mkdir -p tempdir/static
 
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python" >> tempdir/Dockerfile
+echo "FROM python" > tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
-# Cambiamos el EXPOSE a 9999 para que coincida con tu Python
 echo "EXPOSE 9999" >> tempdir/Dockerfile
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t sampleapp .
-# Volvemos a mapear 9999:9999
 docker run -t -d -p 9999:9999 --name samplerunning sampleapp
 docker ps -a
