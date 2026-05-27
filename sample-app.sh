6893082ba3a66b4198daad1a56b4cf5751524df0
NetAcad DEVASC <changeme@example.com>
2026-05-27 19:45:37 +0000

    Bypass a bug de Docker: comando directo en docker run

--- sample-app.sh
+++ sample-app.sh
@@ -6,18 +6,16 @@ cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-cat << 'DOCKERFILE' > tempdir/Dockerfile
-FROM python:3.9
-RUN pip install -q --no-cache-dir --disable-pip-version-check flask
-COPY  ./static /home/myapp/static/
-COPY  ./templates /home/myapp/templates/
-COPY  sample_app.py /home/myapp/
-EXPOSE 9999
-CMD ["python", "/home/myapp/sample_app.py"]
-DOCKERFILE
+echo "FROM python:3.9" > tempdir/Dockerfile
+echo "RUN pip install -q --no-cache-dir --disable-pip-version-check flask" >> tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 9999" >> tempdir/Dockerfile
 
 cd tempdir
-docker build --no-cache -t sampleapp .
+docker build -t sampleapp .
 docker rm -f samplerunning 2>/dev/null || true
-docker run -t -d -p 9999:9999 --name samplerunning sampleapp
+# AQUÍ ESTÁ EL TRUCO: Le decimos qué ejecutar directamente al final de la línea
+docker run -t -d -p 9999:9999 --name samplerunning sampleapp python /home/myapp/sample_app.py
 docker ps -a
